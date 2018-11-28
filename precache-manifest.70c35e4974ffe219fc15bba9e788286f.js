2c523ca5bf49c91d7ef577edae89c66555ecb57e
Tomasz Pach <tomaszpach@icloud.com>
2018-11-28 11:40:59 +0100

    Updates

---
+++ precache-manifest.70c35e4974ffe219fc15bba9e788286f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chat-client/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "41a63393f48a70652e9d",
+    "url": "/chat-client/static/js/runtime~main.41a63393.js"
+  },
+  {
+    "revision": "7e3a2fc18335d8ac9522",
+    "url": "/chat-client/static/js/main.7e3a2fc1.chunk.js"
+  },
+  {
+    "revision": "972826103ae91d3c4ac9",
+    "url": "/chat-client/static/js/1.97282610.chunk.js"
+  },
+  {
+    "revision": "7e3a2fc18335d8ac9522",
+    "url": "/chat-client/static/css/main.b758d933.chunk.css"
+  },
+  {
+    "revision": "7260e61bb856a772fc9422fa199bd9d4",
+    "url": "/chat-client/index.html"
+  }
+];
